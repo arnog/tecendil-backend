0c1be89a77eadfb740f31a2c63044b5146547054
arnog <arno@arno.org>
2021-02-22 14:02:45 +0100

    chore: minor updates

--- README.md
+++ README.md
@@ -4,18 +4,29 @@ The backend component of Tecendil which provides dictionary definitions.
 
 [![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
 
+## API
+
+```
+/define/:word
+```
+
+Returns a JSON record containing an array of entries matching the specified word.
+
 ## Running Locally
 
 Make sure you have [Node.js](http://nodejs.org/) and the [Heroku Toolbelt](https://toolbelt.heroku.com/) installed.
 
 ```sh
-# $ git clone git@github.com:heroku/node-js-getting-started.git # or clone your own fork
-$ cd tengwar
-$ npm install
-$ npm start
+# $ git clone git@github.com:heroku/node-js-getting-started.git
+# or clone your own fork
+cd tengwar
+npm install
+npm start
 ```
 
 The app should now be running on [localhost:5000](http://localhost:5000/).
+
+Try `http://localhost:5000/define/gandalf`.
 
 ## Deploying to Heroku
 
@@ -24,11 +35,3 @@ $ heroku create
 $ git push heroku master
 $ heroku open
 ```
-
-## API
-```
-/define/:word
-```
-
-Returns a JSON record containing an array of entries matching the specified word.
-
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 "use strict";
 
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
-var fs = require("fs");
-var xmlDoc = require("xmldoc").XmlDocument;
-var ASCIIFolder = require("./ascii-folder");
-var asciiFolder = new ASCIIFolder();
-var doc;
+const fs = require("fs");
+const xmlDoc = require("xmldoc").XmlDocument;
+const ASCIIFolder = require("./ascii-folder");
+const asciiFolder = new ASCIIFolder();
+let doc;
 
 /**
  * Return a short-definition (gloss) for a term (lemma)
@@ -24,7 +24,7 @@ function getShortDefinition(q) {
       getShortDefinition.cache = null;
     }, 5 * 60 * 60 * 1000);
   }
-  var result = getShortDefinition.cache[q];
+  let result = getShortDefinition.cache[q];
   if (!result) {
     // console.time("short def for " + q);
     result = getShortDefinitionRecursive(doc, q);
@@ -40,7 +40,7 @@ function getShortDefinition(q) {
  * Return a short gloss for the lemma `q`, starting at node `r`
  */
 function getShortDefinitionRecursive(r, q) {
-  var result = "";
+  let result = "";
   r.eachChild((n) => {
     if (n.name === "word" && n.attr.v === q) {
       result = n.attr.gloss;
@@ -61,8 +61,8 @@ function getShortDefinitionRecursive(r, q) {
  * @param {string} q
  */
 function eachWord(w, q) {
-  var result = [];
-  var pos = w.attr.speech || "";
+  let result = [];
+  const pos = w.attr.speech || "";
 
   if (
     w.name === "word" &&
@@ -74,9 +74,9 @@ function eachWord(w, q) {
     )
   ) {
     // Score how well the current node matches the query
-    var score = 0;
-    var i = 0;
-    var v = w.attr.vfold;
+    let score = 0;
+    let i = 0;
+    let v = w.attr.vfold;
     if (!v) {
       // Cache an ascii and case folded version of the lemma
       w.attr.vfold = asciiFolder.fold(w.attr.v).toLowerCase();
@@ -95,7 +95,7 @@ function eachWord(w, q) {
       }
     }
     if (score === 0 && w.attr.gloss) {
-      var g = w.attr.glossfold;
+      let g = w.attr.glossfold;
       if (!g) {
         // Cache an ascii and case folded version of the gloss
         w.attr.glossfold = asciiFolder.fold(w.attr.gloss).toLowerCase();
@@ -104,10 +104,10 @@ function eachWord(w, q) {
       if (g === q) {
         score = 100;
       } else {
-        var gscore = 0; // Score for a gloss entry
+        let gscore = 0; // Score for a gloss entry
         g.replace(/;/g, ",")
           .split(",")
-          .map(function (entry) {
+          .map((entry) => {
             entry = entry.trim();
             i = entry.indexOf(q);
             if (i === 0) {
@@ -124,7 +124,7 @@ function eachWord(w, q) {
 
     // If this node matches, record the result
     if (score > 0) {
-      var v = w.attr.v;
+      let v = w.attr.v;
       if (v && w.attr.tengwar) {
         // The tengwar attribute contains a thorn or tilde-n to indicate that
         // súlë or noldo should be used instead of s/n
@@ -136,8 +136,8 @@ function eachWord(w, q) {
           v = v.replace("S", "Þ");
         }
       }
-      var notes = w.childNamed("notes");
-      var elements = w
+      const notes = w.childNamed("notes");
+      const elements = w
         .childrenNamed("element")
         .map((x) => x.attr.v)
         .filter((x) => x !== undefined)
@@ -146,15 +146,15 @@ function eachWord(w, q) {
         });
       result = [
         {
-          v: v,
+          v,
           score: Math.round(score),
           language: w.attr.l,
-          pos: pos,
+          pos,
           gloss: w.attr.gloss,
           stem: w.attr.stem,
           notes: notes ? notes.val : undefined,
           tengwar: w.attr.tengwar,
-          elements: elements,
+          elements,
         },
       ];
     }
@@ -168,15 +168,17 @@ function eachWord(w, q) {
   return result;
 }
 
-let gCache = {};
+// gCache is a permanent, preloaded cache
+const gCache = new Map();
+// gRing is a dynamic fixed-size cache
 const RING_SIZE = 40;
-let gRing = new Array(RING_SIZE);
+const gRing = new Array(RING_SIZE);
 
-gRing.ringIndex = 0;
+let gRingIndex = 0;
 
 function addToRing(k, v) {
-  gRing[gRing.ringIndex] = { key: k, value: v };
-  gRing.ringIndex = (gRing.ringIndex + 1) % gRing.length;
+  gRing[gRingIndex] = { key: k, value: v };
+  gRingIndex = (gRingIndex + 1) % RING_SIZE;
 }
 
 function getFromRing(k) {
@@ -187,20 +189,20 @@ function getFromRing(k) {
       // Found a matching key in the ring buffer
       result = gRing[i].value;
 
-      if (gRing.ringIndex !== i) {
-        gRing[gRing.ringIndex] = gRing[i];
+      if (gRingIndex !== i) {
+        gRing[gRingIndex] = gRing[i];
         // Remove the key/value pair from its previous location
         gRing.splice(i, 1);
 
         // Adjust the ring index if the removed element was after it
-        if (i > gRing.ringIndex) {
-          gRing.ringIndex = (gRing.ringIndex + 1) % gRing.length;
+        if (i > gRingIndex) {
+          gRingIndex = (gRingIndex + 1) % gRing.length;
         }
 
         // Insert an empty element at the new ring buffer index
-        gRing.splice(gRing.ringIndex, 0, undefined);
+        gRing.splice(gRingIndex, 0, undefined);
       } else {
-        gRing.ringIndex = (gRing.ringIndex + 1) % gRing.length;
+        gRingIndex = (gRingIndex + 1) % gRing.length;
       }
     }
     i++;
@@ -212,10 +214,10 @@ function getFromRing(k) {
  * Debugging function to display the content of the ring
  */
 function dumpRing() {
-  var result = "";
-  var i = 0;
+  let result = "";
+  let i = 0;
   while (i < gRing.length) {
-    if (i === gRing.ringIndex) result += ">";
+    if (i === gRingIndex) result += ">";
     result += gRing[i] ? gRing[i].key : "?";
     result += " ";
     i++;
@@ -224,18 +226,18 @@ function dumpRing() {
 }
 
 // Load up the dictionary
-console.time("read dictionary");
+console.time("Read dictionary");
 fs.readFile(__dirname + "/eldamo-data.xml", "utf8", (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.timeEnd("read dictionary");
-  console.time("parse dictionary");
+  console.timeEnd("Read dictionary");
+  console.time("Parse dictionary");
   doc = new xmlDoc(data);
-  console.timeEnd("parse dictionary");
+  console.timeEnd("Parse dictionary");
 
-  console.time("cache preload");
+  console.time("Preload cache");
   // Preload a cache
   [
     "gondor",
@@ -292,10 +294,10 @@ fs.readFile(__dirname + "/eldamo-data.xml", "utf8", (err, data) => {
     "ara",
     "arag",
     "aragor",
-  ].forEach((x) => (gCache[x] = JSON.stringify(eachWord(doc, x))));
-  console.timeEnd("cache preload");
+  ].forEach((x) => gCache.set(x, JSON.stringify(eachWord(doc, x))));
+  console.timeEnd("Preload cache");
 
-  console.log("ready");
+  console.log("Ready");
 });
 
 // Startup the app
@@ -307,24 +309,23 @@ app.get("/define/:word", (req, res) => {
   res.header("Access-Control-Allow-Methods", "GET");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
 
-  var q = asciiFolder.fold(req.params.word).toLowerCase();
+  let q = asciiFolder.fold(req.params.word).toLowerCase();
 
-  if (gCache[q]) {
-    console.time("define (cached): " + q);
+  if (gCache.has(q)) {
+    console.log(`Definition "${q}" from cache`);
     res.setHeader("Content-Type", "application/json");
-    res.send(gCache[q]);
-    console.timeEnd("define (cached): " + q);
+    res.send(gCache.get(q));
     return;
   }
 
-  var r = getFromRing(q);
+  let r = getFromRing(q);
   if (r) {
-    console.log("define (ring): " + q);
+    console.log(`Definition "${q}" from ring`);
   } else {
-    console.time("define: " + q);
+    console.time(`Definition "${q}"`);
     r = JSON.stringify(eachWord(doc, q));
     addToRing(q, r);
-    console.timeEnd("define: " + q);
+    console.timeEnd(`Definition "${q}"`);
   }
 
   dumpRing();
